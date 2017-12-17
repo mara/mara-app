8ab405f7ce69bd699e77d843f021b93ec49f3a7d
Martin Loetzsch <martin.loetzsch@gmail.com>
2017-12-18 00:47:39 +0100

    adapt to new mara-db package

--- mara_app/cli.py
+++ mara_app/cli.py
@@ -1,13 +1,14 @@
 """Mara admin command line interface"""
 
-import click, sys
-from mara_app import migrations
-from mara_db import dbs
+import click
 import mara_db.config
+import mara_db.sqlalchemy
+import sys
+from mara_app import migrations
 
 
 @click.command()
 def migrate():
     """Compares the current database with all defined models and applies the diff"""
-    if not migrations.auto_migrate(dbs.engine(mara_db.config.mara_db_alias())):
+    if not migrations.auto_migrate(mara_db.sqlalchemy.engine(mara_db.config.mara_db_alias())):
         sys.exit(-1)
