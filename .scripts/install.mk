cca6ef2170a7a1861d8a2b5ec4567a61821357b1
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-11-29 18:18:30 +0100

    minor: un-indent comments so that they are treated as Makefile comments and not (printed) bash comments

--- .scripts/install.mk
+++ .scripts/install.mk
@@ -21,10 +21,12 @@ update-packages:
 	make -j check-for-unpushed-package-changes .venv/bin/python
 	PYTHONWARNINGS="ignore" .venv/bin/pip install --requirement=requirements.txt --src=./packages --upgrade --exists-action=w
 	make -j check-for-inconstent-package-dependencies .copy-mara-app-scripts
-	# write freeze file
-	# pkg-ressources is automatically added on ubuntu, but breaks the install.
-	# https://stackoverflow.com/a/40167445/1380673
+
+# write freeze file
+# pkg-ressources is automatically added on ubuntu, but breaks the install.
+# https://stackoverflow.com/a/40167445/1380673
 	.venv/bin/pip freeze | grep -v "pkg-resources" > requirements.txt.freeze
+
 	make check-for-newer-package-versions
 	make migrate-mara-db
 	echo -e "\033[32msucceeded, please check output above for warnings\033[0m"
@@ -32,22 +34,26 @@ update-packages:
 
 # create or update virtualenv
 .venv/bin/python: 
-	# if .venv is already a symlink, don't overwrite it
+# if .venv is already a symlink, don't overwrite it
 	mkdir -p .venv
-	# go into the new dir and build it there as venv doesn't work if the target is a symlink
+
+# go into the new dir and build it there as venv doesn't work if the target is a symlink
 	cd .venv && $(.PYTHON3) -m venv --copies --prompt='[$(shell basename `pwd`)/.venv]' .
-	# set environment variables
+
+# set environment variables
 	echo export FLASK_DEBUG=1 >> .venv/bin/activate
 	echo export FLASK_APP=$(shell pwd)/app/app.py >> .venv/bin/activate
-	# add the project directory to path
+# add the project directory to path
 	echo $(shell pwd) > `echo .venv/lib/*/site-packages`/mara-path.pth
-	# install minimum set of required packages
-	# wheel needs to be early to be able to build wheels
+
+# install minimum set of required packages
+# wheel needs to be early to be able to build wheels
 	.venv/bin/pip install --upgrade wheel requests setuptools pipdeptree
-	# Workaround problems with un-vendored urllib3/requests in pip on ubuntu/debian
-	# This forces .venv/bin/pip to use the vendored versions of urllib3 from the installed requests version
-	# see https://stackoverflow.com/a/46970344/1380673
-	-rm -v .venv/share/python-wheels/{requests,chardet,urllib3}-*.whl
+
+# Workaround problems with un-vendored urllib3/requests in pip on ubuntu/debian
+# This forces .venv/bin/pip to use the vendored versions of urllib3 from the installed requests version
+# see https://stackoverflow.com/a/46970344/1380673
+	-rm -vf .venv/share/python-wheels/{requests,chardet,urllib3}-*.whl
 
 
 # auto-migrate the mara db
